f43934f553873ea0712d90d03c16b8bae4f41f01
gss-patricia <gss.patricia@gmail.com>
2024-04-21 21:52:13 +0100

    ajustando prefetch

--- src/hooks/useFetchReplies.js
+++ src/hooks/useFetchReplies.js
@@ -1,20 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 
-export const fetchReplies = async (commentId) => {
-  const response = await fetch(
-    `http://localhost:3000/api/comment/${commentId}/replies`
-  );
-  if (!response.ok) {
-    throw new Error("A resposta da rede não está ok");
-  }
-  return response.json();
-};
-
 export const useFetchReplies = (commentId) => {
   return useQuery({
     queryKey: ["replies", commentId],
-    queryFn: fetchReplies(commentId),
-    staleTime: 1000 * 60 * 5,
+    queryFn: async () => {
+      const response = await fetch(`/api/comment/${commentId}/replies`);
+      if (!response.ok) {
+        throw new Error("A resposta da rede não está ok");
+      }
+      return response.json();
+    },
     retry: 3,
     enabled: !!commentId, //garantindo que só será chamado quando tiver o commentId
   });
