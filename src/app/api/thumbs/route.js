8fb9d1ecba78245e02c4b58a7aaed33112ef3f99
gss-patricia <gss.patricia@gmail.com>
2024-04-23 09:15:20 +0100

    api from main

--- src/app/api/thumbs/route.js
+++ src/app/api/thumbs/route.js
@@ -1,6 +1,6 @@
 import db from "../../../../prisma/db";
 
-// Função que simula um delay
+// Function to simulate a delay
 function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
@@ -8,27 +8,58 @@ function delay(time) {
 export async function POST(request) {
   const { slug } = await request.json();
 
-  // Simula um delay antes de executar a atualização
+  // Simulate a delay before executing the update
   await delay(50);
 
-  await db.post.update({
-    where: {
-      slug,
-    },
-    data: {
-      likes: {
-        increment: 1,
-      },
-    },
-  });
+  // Introduce a condition to simulate an error
+  if (slug === "vuejs-para-iniciantes") {
+    // Assuming 'error-slug' triggers an error
+    return new Response(
+      JSON.stringify({ message: "Simulated error: Invalid slug" }),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 
-  return new Response(
-    JSON.stringify({ message: "Thumbs up incremented successfully!" }),
-    {
-      status: 201,
-      headers: {
-        "Content-Type": "application/json",
+  try {
+    await db.post.update({
+      where: {
+        slug,
       },
-    }
-  );
+      data: {
+        likes: {
+          increment: 1,
+        },
+      },
+    });
+
+    return new Response(
+      JSON.stringify({ message: "Thumbs up incremented successfully!" }),
+      {
+        status: 201,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    // Catch any database or other errors and return an error response
+    console.error("Failed to increment likes:", error);
+    return new Response(
+      JSON.stringify({
+        message: "Failed to increment likes",
+        error: error.message,
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
