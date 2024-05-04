58efb2e1fbd58b590e2a9c499f35ff7dc9ec2e36
gss-patricia <gss.patricia@gmail.com>
2024-05-04 19:22:24 +0100

    video 5.1

--- src/hooks/useFetchReplies.js
+++ src/hooks/useFetchReplies.js
@@ -15,5 +15,7 @@ export const useFetchReplies = ({ commentId, slug }) => {
     queryKey: ["replies", commentId],
     queryFn: async () => fetchReplies({ commentId, slug }),
     enabled: !!commentId && !!slug,
+    retry: 5,
+    //retryDelay: 1000,
   });
 };
